084458430d3fa89bfc731c78949da6fbdd6b7253
Sathvik Tumati <sats.tumati@gmail.com>
2026-05-24 23:41:53 +0530

    .

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -118,7 +118,7 @@ export default function Home() {
                 className="hero__contact-link"
               >
                 <IconBrandGithub size={13} stroke={1.5} />
-                github/Sathvik-Tumati
+                linkedin.com/in/sathvik-tumati/
               </a>
             </div>
           </motion.div>
