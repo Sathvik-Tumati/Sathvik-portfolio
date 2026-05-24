be3794dfc126bcba3ff50f5b9333d5552f84f102
Sathvik Tumati <sats.tumati@gmail.com>
2026-05-24 23:47:33 +0530

    .

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,7 @@
 import { useState, useCallback, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import { motion, AnimatePresence } from 'framer-motion';
-import { IconMail, IconBrandGithub } from '@tabler/icons-react';
+import { IconMail, IconBrandLinkedin } from '@tabler/icons-react';
 import ScrollReveal from '../components/ScrollReveal';
 import PortraitA from '../components/PortraitA';
 import ProjectVisual from '../components/ProjectVisual';
